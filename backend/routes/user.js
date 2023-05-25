a59443a662be214993fb6b68542174e598804c42
RakeshG <rockingrakeshshetty@gmail.com>
2023-05-25 22:23:09 +0530

    146._Add_to_search_history

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -20,6 +20,8 @@ const {
   acceptRequest,
   unfriend,
   deleteRequest,
+  search,
+  addToSearchHistory,
 } = require("../controllers/user");
 const { authUser } = require("../middlwares/auth");
 
@@ -44,5 +46,7 @@ router.put("/unfollow/:id", authUser, unfollow);
 router.put("/acceptRequest/:id", authUser, acceptRequest);
 router.put("/unfriend/:id", authUser, unfriend);
 router.put("/deleteRequest/:id", authUser, deleteRequest);
+router.post("/search/:searchTerm", authUser, search);
+router.put("/addToSearchHistory", authUser, addToSearchHistory);
 
 module.exports = router;
