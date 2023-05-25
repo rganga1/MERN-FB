ed976f47bb2afa0f09f07f876b5a9622f96f2600
RakeshG <rockingrakeshshetty@gmail.com>
2023-05-25 19:33:47 +0530

    37._Register_form

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,13 +1,14 @@
 import "./style.css";
 import LoginForm from "../../components/login/LoginForm";
 import Footer from "../../components/login/Footer";
+import RegisterForm from "../../components/login/RegisterForm";
 
 export default function Login() {
   return (
     <div className="login">
       <div className="login_wrapper">
         <LoginForm />
-        <div className="register"></div>
+        <RegisterForm />
         <Footer />
       </div>
     </div>
