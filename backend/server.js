9a35258d2328c90e48b3cd58f177cb19cf5ea55e
RakeshG <rockingrakeshshetty@gmail.com>
2023-05-25 19:30:13 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { readdirSync } = require("fs");
+const app = express();
+app.use(cors());
+
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}..`);
+});
